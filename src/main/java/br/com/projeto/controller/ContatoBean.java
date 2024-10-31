6af8aec416dae234107cc5be259fc898ec06df20
Daniel Coelho <daniel.coelho.santos@gmail.com>
2024-10-31 13:57:09 -0300

    Foi refatorado com o método de redirecionamento para para a tela de cadastro. Também foi desenvolvido os métodos para redirecionamento para tela home, após cadastrar e o método para limpar o formulário.

--- src/main/java/br/com/projeto/controller/ContatoBean.java
+++ src/main/java/br/com/projeto/controller/ContatoBean.java
@@ -1,13 +1,19 @@
 package br.com.projeto.controller;
 
+import br.com.projeto.controller.service.UsuarioServiceImpl;
 import br.com.projeto.model.Contato;
+import br.com.projeto.model.Usuario;
 import br.com.projeto.model.dao.UsuarioDAO;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.transaction.Transactional;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Named
 @RequestScoped
@@ -18,7 +24,46 @@ public class ContatoBean implements Serializable {
     @Inject
     private UsuarioDAO usuarioDAO;
 
+    private Contato contato;
+
+    @Inject
+    private UsuarioServiceImpl usuarioService;
+
+    private static final Logger logger = Logger.getLogger(ContatoBean.class.getName());
+
+    private final String CONTEXTPATH = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+
+    public ContatoBean() {
+        this.contato = new Contato();
+    }
+
+    public Contato getContato() {
+        return contato;
+    }
+
+    public void setContato(Contato contato) {
+        this.contato = contato;
+    }
+
     public List<Contato> getContatos() {
         return usuarioDAO.getContacts(loginBean.getUsuarioLogado().getId());
+    }
+
+    public void redirectCadastro() throws IOException {
+        logger.info("Redirecionando para tela de cadastro!");
+        FacesContext.getCurrentInstance().getExternalContext().redirect(this.CONTEXTPATH + "/pages/contato/novo.xhtml");
+    }
+
+    @Transactional
+    public void novo() throws IOException {
+        Usuario usuarioLogado = loginBean.getUsuarioLogado();
+        this.contato.setUsuario(usuarioLogado);
+        this.usuarioService.salvar(this.contato);
+        limparFormulario();
+        FacesContext.getCurrentInstance().getExternalContext().redirect(this.CONTEXTPATH + "/pages/home/home.xhtml");
+    }
+
+    public void limparFormulario(){
+        this.contato = new Contato();
     }
 }
